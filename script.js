427c1d616b40d8cedfe4dc6dce7c2225138dc691
rajmalpure <malpure.raj@kalvium.community>
2024-02-03 09:24:05 +0530

    js added

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-var listtoActivities = [];
+var listofActivities = [];
 
 var input = document.getElementById("input");
 
-var todolist = document.getElementById("todolist");
+var todoList = document.getElementById("todolist");
 
 document.getElementById("button").onclick = click;
 
-//Event Handler vs Event Listenr 
-
 function click() {
-    listtoActivities.push(input.value);
-    console.log(listtoActivities);
+    listofActivities.push(input.value);
 
     input.value = "";
     showList();
 }
 
 function showList(){
-    todolist.innerHTML = " ";
+    todoList.innerHTML = " ";
 
-    listtoActivities.forEach(function(n,i){
-        todolist.innerHTML +="<li>" + n + "<a onclick='editItem(" + i + ")'>Edit</a>" +
+    listofActivities.forEach(function(n, i){
+        todoList.innerHTML += "<li>" + n + "<a onclick='editItem(" + i + ")'>Edit</a>" + 
         "<a onclick='deleteItem(" + i + ")'>&times | </a></li>";
-
     })
-
+    //<li>n<a onclick='editItem(i)'>Edit</a><a onclick='deleteItem(i)'>&times | </a></li>
 }
 
 function deleteItem(i){
-    listtoActivities.splice(i,1);
-    showList()
+    listofActivities.splice(i,1);
+    showList();
 }
 
 function editItem(i){
-    var newValue = promt("Please insert your new value");
-    listtoActivities.splice(i,1,newValue);
+    var newValue = prompt("Please insert your new value");
+    listofActivities.splice(i, 1, newValue);
     showList();
 }
